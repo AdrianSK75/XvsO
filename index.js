7a911930e0c89952c6acc781259f3bc291cd3903
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2021-05-15 23:02:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,109 +1,90 @@
 var xao = "X";
-             var arr = new Array(9).fill(0);
-             var pos = null;
-             for(var btn = 0; btn < 9; ++btn)
-                   document.querySelectorAll("#numbers")[i].innerHTML = ' ';
+var arr = new Array(9).fill(0);
+var pos = null;
+
+for(var btn = 0; btn < 9; ++btn)
+      document.querySelectorAll("#numbers")[i].innerHTML = ' ';
              
-             function setTic(pos) {
-                document.querySelector("#title").innerHTML = xao + " TURN.";
-                 if(xao == "X") {
-                     arr[pos] = 1;                   
-                     document.querySelectorAll("#numbers")[pos].innerHTML = xao = "O";
-                 } else {
-                    arr[pos] = 2;
-                    document.querySelectorAll("#numbers")[pos].innerHTML = xao = "X";
+function setTic(pos) {
+          document.querySelector("#title").innerHTML = xao + " TURN.";
+           
+          if(xao == "X") {
+               arr[pos] = 1;                   
+               document.querySelectorAll("#numbers")[pos].innerHTML = xao = "O";
+            
+           } else {
+                arr[pos] = 2;
+                document.querySelectorAll("#numbers")[pos].innerHTML = xao = "X";
 
-                 }
+            }
                  
-                 checkTable();
-             }
+            checkTable();
+}
 
-             function checkTable() {
-                      var check = 1, item = 'O', winner; 
-                      while(check <= 2) {
-                            //Horizontal
-                            if(arr[0] == check && arr[1] == check && arr[2] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[0].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[1].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[2].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            if(arr[3] == check && arr[4] == check && arr[5] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[3].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[5].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            if(arr[6] == check && arr[7] == check && arr[8] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[6].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[7].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[8].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            //Vertical
-                            if(arr[0] == check && arr[3] == check && arr[6] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[0].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[3].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[6].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            if(arr[1] == check && arr[4] == check && arr[7] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[1].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[7].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            if(arr[2] == check && arr[5] == check && arr[8] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[2].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[5].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[8].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            //Diagonals
-                            if(arr[0] == check && arr[4] == check && arr[8] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[0].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[8].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
-                            if(arr[2] == check && arr[4] == check && arr[6] == check) {
-                                winner = check;
-                                document.querySelectorAll(".button")[2].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
-                                document.querySelectorAll(".button")[6].style.background = "#d8f8b7";
-                                checkWinner(winner);
-                                break;
-                            }
+function checkTable() {
+          var check = 1, item = 'O'; 
+          while(check <= 2) {               
+                if(arr[0] == check && arr[1] == check && arr[2] == check) {  //Horizontal                             
+                   document.querySelectorAll(".button")[0].style.background = "#d8f8b7";
+                   document.querySelectorAll(".button")[1].style.background = "#d8f8b7";
+                   document.querySelectorAll(".button")[2].style.background = "#d8f8b7";
+                   checkWinner(check);
+                    
+                } else if(arr[3] == check && arr[4] == check && arr[5] == check) {            
+                          document.querySelectorAll(".button")[3].style.background = "#d8f8b7";
+                          document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
+                          document.querySelectorAll(".button")[5].style.background = "#d8f8b7";
+                          checkWinner(check);
+                      
+                 } else if(arr[6] == check && arr[7] == check && arr[8] == check) {
+                          document.querySelectorAll(".button")[6].style.background = "#d8f8b7";
+                          document.querySelectorAll(".button")[7].style.background = "#d8f8b7";
+                          document.querySelectorAll(".button")[8].style.background = "#d8f8b7";
+                          checkWinner(check);
+                      
+                 } else if(arr[0] == check && arr[3] == check && arr[6] == check) { //Vertical
+                           document.querySelectorAll(".button")[0].style.background = "#d8f8b7";
+                           document.querySelectorAll(".button")[3].style.background = "#d8f8b7";
+                           document.querySelectorAll(".button")[6].style.background = "#d8f8b7";
+                           checkWinner(check);
+                        
+                  } else if(arr[1] == check && arr[4] == check && arr[7] == check) {
+                            document.querySelectorAll(".button")[1].style.background = "#d8f8b7";
+                            document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
+                            document.querySelectorAll(".button")[7].style.background = "#d8f8b7";
+                            checkWinner(check);
+                                
+                  } else if(arr[2] == check && arr[5] == check && arr[8] == check) {
+                            document.querySelectorAll(".button")[2].style.background = "#d8f8b7";
+                            document.querySelectorAll(".button")[5].style.background = "#d8f8b7";
+                            document.querySelectorAll(".button")[8].style.background = "#d8f8b7";
+                            checkWinner(check);
+
+                   } else if(arr[0] == check && arr[4] == check && arr[8] == check) { //Diagonals                             
+                             document.querySelectorAll(".button")[0].style.background = "#d8f8b7";
+                             document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
+                             document.querySelectorAll(".button")[8].style.background = "#d8f8b7";
+                             checkWinner(check);
+                     
+                   }   else if(arr[2] == check && arr[4] == check && arr[6] == check) {
+                               document.querySelectorAll(".button")[2].style.background = "#d8f8b7";
+                               document.querySelectorAll(".button")[4].style.background = "#d8f8b7";
+                               document.querySelectorAll(".button")[6].style.background = "#d8f8b7";
+                               checkWinner(check);
+ 
+                   }
                         ++check;
                         item = 'X';
-                    }
-              
-             }
-            function checkWinner(winner) {
-                for(var btn = 0; btn < 9; ++btn) {
-                    document.querySelectorAll(".button")[btn].disabled = true;
-                }
+            }
+}
+function checkWinner(winner) {
+          for(var btn = 0; btn < 9; ++btn) {
+               document.querySelectorAll(".button")[btn].disabled = true;
+           }
                 
-                if(winner == 1) {
-                    document.querySelector("#title").innerHTML = "THE WINNER IS O !";
-                } else {
-                    document.querySlector("#title").innerHTML = "THE WINNER IS X !";
-                }
+           if(winner == 1) {
+               document.querySelector("#title").innerHTML = "THE WINNER IS O !";
+            } else {
+               document.querySlector("#title").innerHTML = "THE WINNER IS X !";
             }
-            function resetGame() {
-                location.reload();
-            }
+}
